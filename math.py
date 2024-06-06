94941b011c01a7657caf6db85727eb037154820b
ArtJR2003 <163406569+ArtJR2003@users.noreply.github.com>
2024-06-06 23:21:12 +0400

    Add files via upload

---
+++ math.py
@@ -0,0 +1,4 @@
+x = int(input("Enter your first number: "))
+y = int(input("Enter your second number: "))
+sum = (x - y) / (x + y)
+print("Your answer is:", sum)
---
+++ weight.py
@@ -0,0 +1,3 @@
+weight = int(input("Enter your weight in kilograms: "))
+pounds = weight * 2.2
+print("Your weight in pounds is:",  pounds)
